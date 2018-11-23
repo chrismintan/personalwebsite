30f493357ccf0454e74a0881504a219666b967da
chrismintan <chris_acs@hotmail.com>
2018-11-23 12:21:17 +0800

    Updates

---
+++ js/project-demo.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+	console.log( "ready!" );
+
+	$("#testingtime").click(function(){
+		console.log('Clicked!')
+	})
+
+	document.getElementById("testingtime").addEventListener('click', function(){
+		console.log('Clicker')
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
